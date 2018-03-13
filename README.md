535a12b322f95dd10267ce9f5594fe277e2ac459
R-Mckenzie <rossmckenzie11@gmail.com>
2018-03-13 01:21:28 +0000

    adds player kryboard controls

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Platform Hero
 A simple platformer game I'm working on to learn Javascript. When it's finished, it will contain levels, enemies, lives, and a player score.
-
-The game can be played by downloading the files and opening the [index.html](index.html) with a browser.
--- game.js
+++ game.js
@@ -1,9 +1,16 @@
 let config={
   type: Phaser.AUTO,
-  width: 600,
-  height: 400,
+  width: 640,
+  height: 384,
   parent: 'game',
   backgroundColor: '#ffffff',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
   scene:{
     preload: preload,
     create: create,
@@ -17,10 +24,30 @@ function preload(){
   this.load.image('player', 'assets/stickman.png');
 }
 
+let player;
+let cursors;
+let controls;
 function create(){
-  this.add.image(16, 16, 'player');
+  player=this.physics.add.sprite(100, 100, 'player');
+  player.setCollideWorldBounds(true);
+  controls=this.input.keyboard.addKeys({
+    left:Phaser.Input.Keyboard.KeyCodes.A,
+    right:Phaser.Input.Keyboard.KeyCodes.D,
+    jump:Phaser.Input.Keyboard.KeyCodes.SPACE
+  });
 }
 
 function update(){
-
+  if(controls.left.isDown){
+    player.setVelocityX(-160);
+  }
+  else if(controls.right.isDown){
+    player.setVelocityX(160);
+  }
+  else{
+    player.setVelocityX(0);
+  }
+  if(controls.jump.isDown&&player.body.onFloor()){
+    player.setVelocityY(-100);
+  }
 }
